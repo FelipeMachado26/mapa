94fd6e116b09ca7de1a7ea39c11f5ecfd4d4ead5
Felipe <lhipemachado@gmail.com>
2024-08-22 19:44:06 -0300

    configuração do middleware de json

--- Documents/17-Api-Rest/src/index.ts
+++ Documents/17-Api-Rest/src/index.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config'
 import  express  from "express";
 import rotas from './rotas';
+import { json } from 'express';
 
 
 const servidor = express()
+
+servidor.use(express.json())
+
 
 servidor.use(rotas)
 
